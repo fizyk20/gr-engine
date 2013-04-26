8737e1bb24813d195aa7ae12866e77abdc7eeca0
Bartłomiej Kamiński <bartlomiej.kaminski@lacan.com.pl>
2013-04-26 16:53:31 +0200

    Initial commit. ADDED: numerical integrators (only RK4 at the moment)

---
+++ numeric.cpp
@@ -0,0 +1,149 @@
+#include "numeric.h"
+
+// StateLengthError "what" method
+
+const char* StateLengthError::what() const throw()
+{
+	return "State vector length mismatch.";
+}
+
+/*******************************************************************************
+ *
+ *  StateVector class implementation
+ *
+ *******************************************************************************/
+ 
+StateVector StateVector::operator+(const StateVector arg)
+{
+	if(arg.size() != this->size())
+		throw StateLengthError();
+		
+	int i;
+	StateVector result;
+	for(i = 0; i < this->size(); i++)
+		result.push_back((*this)[i] + arg[i]);
+		
+	return result;
+}
+
+StateVector StateVector::operator-(const StateVector arg)
+{
+	if(arg.size() != this->size())
+		throw StateLengthError();
+		
+	int i;
+	StateVector result;
+	for(i = 0; i < this->size(); i++)
+		result.push_back((*this)[i] - arg[i]);
+		
+	return result;
+}
+
+StateVector StateVector::operator*(const double arg)
+{	
+	int i;
+	StateVector result;
+	for(i = 0; i < this->size(); i++)
+		result.push_back((*this)[i]*arg);
+		
+	return result;
+}
+
+StateVector operator*(const double arg, const StateVector arg2)
+{	
+	int i;
+	StateVector result;
+	for(i = 0; i < arg2.size(); i++)
+		result.push_back(arg2[i]*arg);
+		
+	return result;
+}
+
+StateVector StateVector::operator/(const double arg)
+{	
+	int i;
+	StateVector result;
+	for(i = 0; i < this->size(); i++)
+		result.push_back((*this)[i]/arg);
+		
+	return result;
+}
+
+/*******************************************************************************
+ *
+ *  Integrator class implementation
+ *
+ *******************************************************************************/
+ 
+Integrator::Integrator(double t0, double stepSize)
+{
+	t = t0;
+	this->stepSize = stepSize;
+}
+
+Integrator::~Integrator()
+{
+}
+
+void Integrator::setStepSize(double step)
+{
+	stepSize = step;
+}
+
+void Integrator::setDiffEquation(DiffEq* eq)
+{
+	equation = eq;
+}
+
+void Integrator::setState(StateVector v)
+{
+	currentState = v;
+}
+
+void Integrator::setT(double t)
+{
+	this->t = t;
+}
+
+double Integrator::getStepSize()
+{
+	return stepSize;
+}
+
+StateVector Integrator::getState()
+{
+	return currentState;
+}
+
+double Integrator::getT()
+{
+	return t;
+}
+
+/*******************************************************************************
+ *
+ *  RK4Integrator class implementation
+ *
+ *******************************************************************************/
+ 
+StateVector RK4Integrator::next(double step)
+{
+	double h;
+	if(step == 0.0) 
+		h = stepSize;
+	else
+		h = step;
+		
+	StateVector k1, k2, k3, k4;
+	
+	k1 = equation -> derivative(t, currentState);
+	k2 = equation -> derivative(t + h/2, currentState + k1*h/2);
+	k3 = equation -> derivative(t + h/2, currentState + k2*h/2);
+	k4 = equation -> derivative(t + h, currentState + k3*h);
+	
+	currentState = currentState + (k1 + 2*k2 + 2*k3 + k4)*h/6;
+	t += h;
+		
+	return currentState;
+} 
+
---
+++ numeric.h
@@ -0,0 +1,83 @@
+#ifndef __NUMERIC_H__
+#define __NUMERIC_H__
+
+/*! \file numeric.h
+ * \brief Classes implementing numerical methods
+ */
+
+#include <vector>
+#include <exception>
+using namespace std;
+
+/*! \class StateLengthError
+ * \brief An exception class thrown when there is a state vector length mismatch
+ */
+class StateLengthError : public exception
+{
+public:
+	const char* what() const throw();
+};
+
+/*! \class StateVector
+ * \brief State vector class - basically a vector<double> with addition, subtraction and multiplication by doubles
+ */
+class StateVector : public vector<double>
+{
+public:
+	StateVector operator+(const StateVector);
+	StateVector operator-(const StateVector);
+	StateVector operator*(const double);
+	friend StateVector operator*(const double, const StateVector);
+	StateVector operator/(const double);
+};
+
+/*! \class DiffEq
+ * \brief Base class for implementing differential equations.
+ *        Defining differential equations is done by overloading the \a derivative method in a child class
+ */
+class DiffEq
+{
+public:
+	DiffEq() {}
+	virtual ~DiffEq() = 0;
+	
+	virtual StateVector derivative(double t, StateVector v) = 0;
+};
+
+/*! \class Integrator
+ * \brief Base class implementing a numerical integrator.
+ */
+class Integrator
+{
+protected:
+	double t;
+	double stepSize;
+	DiffEq* equation;
+	StateVector currentState;
+public:
+	Integrator(double t0 = 0.0, double stepSize = 0.01);
+	~Integrator();
+	
+	virtual StateVector next(double step = 0.0) = 0;
+	
+	void setStepSize(double);
+	void setDiffEquation(DiffEq*);
+	void setState(StateVector);
+	void setT(double);
+	
+	double getStepSize();
+	StateVector getState();
+	double getT();
+};
+
+/*! \class RK4Integrator
+ * \brief Class implementing an RK4 numerical integrator.
+ */
+class RK4Integrator : public Integrator
+{
+public:
+	StateVector next(double step = 0.0);
+};
+
+#endif
+
